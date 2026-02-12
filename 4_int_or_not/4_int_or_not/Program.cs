e176ec142039512af579464932584abdb7d04d07
TCAPG22503004 <tcapg22503004@gmail.com>
2026-02-12 14:00:53 +0900

    No.4: integer or not

--- 4_int_or_not/4_int_or_not/Program.cs
+++ 4_int_or_not/4_int_or_not/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// input
+var n = Console.ReadLine();
+if (n == null) return;
+
+
+// judge
+bool isInt = true;
+try {
+    int m = int.Parse(n);
+}
+catch {
+    isInt = false;
+}
+
+
+// output
+if (isInt) {
+    Console.WriteLine(n.ToString() + " is integer");
+}
+else {
+    Console.WriteLine(n.ToString() + " is not integer");
+}
+
