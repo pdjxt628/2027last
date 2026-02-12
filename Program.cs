5fc4fabafce176e450511d073537f9e956ba03db
yoshidann <TCAPG22503010@gmail.com>
2026-02-12 15:08:59 +0900

    ②

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str =int.Parse(Console.ReadLine());
+
+            if(str==(int)str)
+            {
+                Console.WriteLine(str+"!整数だ！");
+            }
+            else
+            {
+                Console.WriteLine("整数じゃないです--");
+            }
+                      
+
+        }
+    }
+}
