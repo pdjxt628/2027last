d81e8a0cdc316901bc0470948b14375ba419beb7
arikui0414 <TCAPG22503002@gmail.com>
2026-02-12 15:54:38 +0900

    test

---
+++ 0212/0212/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace App02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("数値を入力してください。");
+
+            int inputNum = Convert.ToInt32(Console.ReadLine());
+            string txt = inputNum.ToString();
+            if (Regex.IsMatch(txt, @"^[0-9]+$")){
+                Console.WriteLine("整数です");
+            }
+            else
+            {
+                Console.WriteLine("整数ではありません");
+            }
+        }
+    }
+}
