1bed9f33437b9c0c2d936acc5d41cab52f0b7cc1
poppukonn <ichiks5293@gmail.com>
2026-02-12 13:57:14 +0900

    int_checker

--- int_checker/Properties/AssemblyInfo.cs
+++ int_checker/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // アセンブリに関する一般的な情報は、次の方法で制御されます。
 // 制御されます。アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更します。
-[assembly: AssemblyTitle("it_last")]
+[assembly: AssemblyTitle("int_checker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("it_last")]
+[assembly: AssemblyProduct("int_checker")]
 [assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
-[assembly: Guid("66a081e6-aa66-4ad0-9c90-3985971f0674")]
+[assembly: Guid("272c12b5-3c59-413f-bcb5-88e5c91b05ec")]
 
 // アセンブリのバージョン情報は次の 4 つの値で構成されています:
 //
