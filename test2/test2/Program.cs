77195947e507387f6a2ec74104b6accb7185906e
yoshidann <TCAPG22503010@gmail.com>
2026-02-12 15:19:58 +0900

    2

--- test2/test2/Program.cs
+++ test2/test2/Program.cs
@@ -5,17 +5,17 @@ namespace test
     {
         static void Main(string[] args)
         {
-            var str =int.Parse(Console.ReadLine());
+            var str = int.Parse(Console.ReadLine());
 
-            if(str==(int)str)
+            if (str == (int)str)
             {
-                Console.WriteLine(str+"!整数だ！");
+                Console.WriteLine(str + "!整数だ！");
             }
             else
             {
                 Console.WriteLine("整数じゃないです--");
             }
-                      
+
 
         }
     }
