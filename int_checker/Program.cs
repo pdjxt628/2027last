950611ef20e27e1512a444919630b2f1bee4c734
riokundazo <ichiks5293@gmail.com>
2026-02-14 00:09:31 +0900

    copy

---
+++ int_checker/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace int_checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true) {
+                Console.WriteLine("数値を入力してください");
+
+                string value = Console.ReadLine();
+                int convert;
+                bool isNumber = int.TryParse(value, out convert);
+
+                if (isNumber) {
+                    Console.WriteLine($"{value}は整数です");
+                }
+                else {
+                    Console.WriteLine($"{value}は整数以外ではありません");
+                }
+
+                    while (true) {
+                        Console.WriteLine("続けますか？(y/n)");
+                        var check = Console.ReadLine();
+
+                        if (check == "y") {
+
+                            break;
+                        }
+                        if (check == "n") {
+                            return;
+                        }
+                    }
+            }
+        }
+    }
+}
---
+++ memo_app_simple/Form1.cs
@@ -0,0 +1,76 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace memo_app
+{
+    public partial class Form1 : Form
+    {
+        private string connStr = "server=172.16.2.26;user id=ichiru;password=GAMEshitai_5293;database=ichiru";
+
+        string inputText;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Load();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Save(inputText);
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            inputText = textBox1.Text;
+        }
+
+        void Save(string content)
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try {
+                conn.Open();
+                DataTable tbl = new DataTable();
+                MySqlDataAdapter dataAdp = new MySqlDataAdapter($"update 2027last_memoapp_simple set content=\"{content}\"", conn);
+                dataAdp.Fill(tbl);
+                conn.Close();
+            }
+            catch (MySqlException mse) {
+                MessageBox.Show(mse.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        void Load()
+        {
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try {
+                conn.Open();
+                DataTable tbl = new DataTable();
+                MySqlDataAdapter dataAdp = new MySqlDataAdapter($"select content from 2027last_memoapp_simple", conn);
+                dataAdp.Fill(tbl);
+                textBox1.Text = tbl.Rows[0][0];
+                conn.Close();
+            }
+            catch (MySqlException mse) {
+                MessageBox.Show(mse.Message, "error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
